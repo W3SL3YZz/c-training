45c8aa16b5c841986a1a5948e2c6e15d7380891c
Wesley Willian <96602025+W3SL3YZz@users.noreply.github.com>
2022-11-30 14:01:31 -0300

    Add files via upload

---
+++ EntregaDoTeste/EntregaDoTeste/wwwroot/js/site.js
@@ -0,0 +1,60 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Seja bem vindo!"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: false
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Somos a melhor fábrica de SoftWare do Brasil!", "Conheça nossos produtos"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPauser: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
